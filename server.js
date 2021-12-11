f50c64983dc5a19f63b8753168159a870ffbc7d2
Kathleen Wine <katie.wine2@gmail.com>
2021-12-10 19:06:17 -0500

    adding workout model

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
-app.use(require("./routes/api.js"));
+// app.use(require("./public/js/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
